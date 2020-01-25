4e8648807918c92ff98392b37ded46a6c3d3182a
yaminigunti <60088148+yaminigunti@users.noreply.github.com>
2020-01-25 09:42:07 +0530

    Add files via upload

--- app.component.spec.ts
+++ app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proj04'`, () => {
+  it(`should have as title 'proj3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('proj04');
+    expect(app.title).toEqual('proj3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('proj04 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('proj3 app is running!');
   });
 });
--- app.component.ts
+++ app.component.ts
@@ -6,52 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private message:string=null;
-  private version :number=null;
-  private topics:string[]=null;
-  private topicname:string=null;
-  private index:number=null;
-  
-  public  constructor(){
-    this.message="welcome Yamini!";
-    this.version=8;
-    this.topics=['Data Binding','Forms','Http','Routing'];
-  }
-  public addTopic(currentTopic:string):boolean {
-    if(currentTopic.length==0){
-      alert('empty items cannot be added');
-      return false;
-    }
-   
-    for(var topic of this.topics){
-      if(currentTopic==topic){
-        alert('topic already exists !!!!! enter new topic')
-      }
-    }
-    this.topics.push(currentTopic) 
-  }
+  private FirstNumber:number=null;
+  private SecondNumber:number=null;
+  private Result:number=null;
 
-  public deleteTopic(idx:number):void {
-     this.topics.splice(idx,1);  
-  } 
-  public deleteTopicName(currentTopic:string):void{
-for(var topic of this.topics){
-if(currentTopic==topic){
-  this.index=this.topics.indexOf(currentTopic);
-  this.topics.splice(this.index,1);
-}  
+
+public getAdd():void {
+  this.Result=this.FirstNumber+this.SecondNumber;
 }
-alert('item is deleted');
+public getSub():void {
+  this.Result=this.FirstNumber-this.SecondNumber;
 }
+public getMul():void {
+  this.Result=this.FirstNumber*this.SecondNumber;
+}
+public getDiv():void {
+  this.Result=this.FirstNumber/this.SecondNumber;
 
-  public searchTopic(currentTopic:string):void {
-    for(var topic of this.topics){
-    if(currentTopic==topic) {
-      this.index=this.topics.indexOf(currentTopic);
-      alert('topic present at'+(this.index+1));
-    } 
-  }
-  alert('topic is not present')
-  }
-
+}
 }
